0f47ce37d55aa9927fd5a3a65d21b75e55de1e5b
Luccanjm <lucnjm@gmail.com>
2021-04-08 13:44:43 -0300

    ajeitando tabelas de acordo com plano da empresa

--- projetopipo/src/componentes/paginacaoBeneficiarios/index.js
+++ projetopipo/src/componentes/paginacaoBeneficiarios/index.js
@@ -7,19 +7,20 @@ import ReactPaginate from 'react-paginate';
 export default function PaginacaoBeneficiarios () {
     const [data, setData] = useState([]);
     const [erroMensagem, setErroMensagem] = useState('');
-    const [selectedPage, setSelectedPage] = useState('');
+    const [selectedPage, setSelectedPage] = useState(<></>);
     const [estado, setEstado] = useState ({
         offset: 1,
         perPage: 4,
         currentPage: selectedPage
     });
+
     const [arrayDados, setArrayDados] = useState(JSON.parse(localStorage.getItem('@pipo:dadosEmpresa')))
         const recebendoData = useCallback( 
         async(e) => {
                 try{
                     const resposta = await api.get(`beneficiarios?nomeEmpresa=${arrayDados.nome}&_limit=${4}&_page=${selectedPage +1}`)
-                    setData(resposta.data);               
-                    
+                    setData(resposta.data);
+      
                 }catch(error){
                     console.log("Erro na busca da API(paginacaoBeneficiarios)", error);
                     setErroMensagem(error);
@@ -35,13 +36,14 @@ export default function PaginacaoBeneficiarios () {
                 offset: offset,
                 perPage:4
                 });
-        
             };
         
 
         useEffect(() => {
             recebendoData();
         },[recebendoData])
+
+
     return (
         <Container>
                 <Table>
@@ -49,11 +51,11 @@ export default function PaginacaoBeneficiarios () {
                         <th className="nomeClasse" >Nome</th>
                         <th>CPF</th>
                         <th className="dtClasse" >Dt.Admissão</th>
-                         <th>E-mail</th> 
-                        <th className="pesoClasse" >Peso(kg)</th> 
-                        <th className="alturaClasse" >Altura(cm)</th> 
-                        <th className="horasClasse">Horas Meditadas últimos 7 dias</th>
-                        <th className="enderecoClasse">Endereço</th> 
+                        {data[0] ? ((data[0].email == "--") ?  null : <th>E-mail</th>) : null }
+                        {data[0] ? ((data[0].peso == "--") ?  null :<th className="pesoClasse" >Peso(kg)</th>)  : null }
+                        {data[0] ? ((data[0].altura == "--") ?  null :<th className="alturaClasse" >Altura(cm)</th>) : null }
+                        {data[0] ? ((data[0].horasMed == "--") ?  null :<th className="horasClasse">Horas Meditadas últimos 7 dias</th>): null }
+                        {data[0] ? ((data[0].endereco == "--") ?  null :<th className="enderecoClasse">Endereço</th>) : null }
                         </thead>
                     <tbody>
                         {data.map((item) =>
@@ -77,11 +79,11 @@ export default function PaginacaoBeneficiarios () {
                                 <td className="nomeClasse"  key={item.nome}>{item.nome}</td>
                                 <td key={item.cpf}>{item.cpf}</td>
                                 <td className="dtClasse" key={item.dtAdmissao}>{item.dtAdmissao}</td>
-                                <td key={item.email}>{item.email}</td> 
-                                <td  className="pesoClasse" key={item.peso}>{item.peso}</td> 
-                                <td  className="alturaClasse" key={item.altura}>{item.altura}</td> 
-                                <td className="horasClasse" key={item.horasMed}>{item.horasMed}</td> 
-                                <td className="enderecoClasse" key={item.endereco}>{item.endereco}</td> 
+                                {(data[0].email == "--") ? null : <td key={item.email}>{item.email}</td> }
+                                {(data[0].planoDentalNome == "--") ? null : <td  className="pesoClasse" key={item.peso}>{item.peso}</td> }
+                                {(data[0].planoDentalNome == "--") ? null : <td  className="alturaClasse" key={item.altura}>{item.altura}</td> }
+                                {(data[0].planoSaudeMentalNome == "--") ? null : <td className="horasClasse" key={item.horasMed}>{item.horasMed}</td> }
+                                {(data[0].endereco == "--") ? null : <td className="enderecoClasse" key={item.endereco}>{item.endereco}</td> }
                             </tr>
                         )}
                                                         
